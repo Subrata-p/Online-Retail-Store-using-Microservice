4a473e6eed751c797d2e17f7719fc5b1c3dadc23
Subrata Paul <81162470+Subrata-p@users.noreply.github.com>
2025-04-12 20:36:18 +0530

    Delete cartservice directory

--- cartservice/src/main/java/com/example/cartservice/repository/LineItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cartservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.cartservice.model.LineItem;
-
-public interface LineItemRepository extends CrudRepository<LineItem, Integer>{
-
-}
